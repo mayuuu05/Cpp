d6dbc3a431b5d8d32bb068df8e2592bca3dd87e2
Mayuri Purohit <149376263+mayuuu05@users.noreply.github.com>
2024-02-12 21:37:29 +0530

    Update Area of the shapes.cpp

--- Heritage/Area of the shapes.cpp
+++ Heritage/Area of the shapes.cpp
@@ -3,4 +3,49 @@
 //  that calculate the area of the shape area (). In the main, define two objects a 
 //  triangle and a rectangle, and then call the area() function using these two objects.
 
+#include<iostream>
+using namespace std;
 
+class Shape
+{
+	protected :
+		int width,height; 
+		public :	
+  	Shape()
+  	{
+  		cout << "Enter Width : ";
+  		cin >> width ;
+  		cout << "Enter height : ";
+  		cin >> height;
+  	}
+  };
+
+class Triangle : public Shape
+{
+	public :
+	void triangleArea()
+	{
+  		int triangleArea =  (width * height) * 1/2 ;
+  		cout << endl << "=> Triangle area is = " << triangleArea << endl << endl;
+	}
+};
+
+class Rectangle : public Shape
+{
+  	public :
+  	void rectangleArea()
+  	{
+    		int rectangleArea = width * height;
+    		cout << endl << "=> Rectangle area is =  " << rectangleArea ;
+  	}
+};
+int main()
+{
+  	Triangle t1;
+  	t1.triangleArea();
+  	
+  	Rectangle r1;
+  	r1.rectangleArea();
+  
+  	return 0;
+}
