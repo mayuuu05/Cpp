69f91446fe132b2dd1b2e914d10c1692326d0245
Mayuri Purohit <149376263+mayuuu05@users.noreply.github.com>
2024-02-15 15:47:46 +0530

    Add files via upload

---
+++ Multiprocessor/Addition with passing argument & overloding.cpp
@@ -0,0 +1,36 @@
+//Q.1 Write a Program to create a Message class with a constructor that takes a single string with 
+//a default value. Create a private member of the typed string, and in the constructor simply assign the 
+//argument string to your internal string. Create two overloaded member functions called print( ): one that 
+//takes no arguments and simply prints the message stored in the variable and one that takes a string argument, 
+//which it prints in addition to the internal message.
+
+#include<iostream>
+using namespace std;
+
+class Message
+{
+	string msg;
+	
+	public :
+		
+		Message(string msg)
+		{
+			this->msg = msg;
+		}
+		void print()
+		{
+			cout << msg << endl << endl;
+		}
+		void print(string newMessage)
+		{
+			cout << msg << " " << newMessage << endl;
+		}
+};
+int main()
+{
+	Message m1("Hey!");
+	
+	m1.print();
+	m1.print("Mayuri");
+	return 0;
+}
