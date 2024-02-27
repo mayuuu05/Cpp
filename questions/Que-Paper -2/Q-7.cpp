272dc8b16cd11d55404b1195fe58fd4df2253b0d
Mayuri Purohit <149376263+mayuuu05@users.noreply.github.com>
2024-02-27 19:31:12 +0530

    Update Q-7.cpp

--- questions/Que-Paper -2/Q-7.cpp
+++ questions/Que-Paper -2/Q-7.cpp
@@ -2,3 +2,66 @@
 // Implement concrete/normal classes `Circle` and `Rectangle` that inherit from `Shape`. 
 // Demonstrate polymorphism by creating an array of `Shape` pointers pointing to objects of both `Circle` and `Rectangle`.
 // Call the `calculateArea()` and `draw()` functions for each object.
+
+#include<iostream>
+using namespace std;
+
+class Shape
+{
+	protected:
+	const float pi=3.14;
+	int r=10;
+	int l=10;
+	int w=20;
+	
+	public :
+	virtual void calculateArea()=0;
+	virtual void draw()=0;
+};
+class Circle : public Shape
+{
+
+	public:
+	virtual void calculateArea()
+	{
+		cout << "\t [Circle]" << endl << endl;
+		cout << "\tCircle area : " << pi*r*r << endl;
+	}
+	virtual void draw()
+	{
+		cout << "\tDrawing circle." << endl << endl;
+	}
+};
+class Rectangle : public Shape
+{
+
+	public:
+	virtual void calculateArea()
+	{
+		cout << "------------------------------------" <<endl;
+		cout <<endl << "\t[Reactangle]" << endl << endl;
+		cout << "\tRectangle area : " << l*w << endl;
+	}
+	virtual void draw()
+	{
+		cout << "\tDrawing rectangle." << endl;
+	}
+};
+
+int main()
+{
+	Shape *s1[2];
+	
+	Circle c1;
+	Rectangle r1;
+	
+	s1[0]=&c1;
+	s1[1]=&r1;
+	
+	for(int i=0 ; i<2 ; i++)
+	{
+		s1[i]->calculateArea();
+		s1[i]->draw();
+	}
+		return 0;
+}
