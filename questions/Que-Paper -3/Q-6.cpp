e75528a0b120bd492f683cc30527ce27fd436956
Mayuri Purohit <149376263+mayuuu05@users.noreply.github.com>
2024-02-28 20:10:13 +0530

    Update Q-6.cpp

--- questions/Que-Paper -3/Q-6.cpp
+++ questions/Que-Paper -3/Q-6.cpp
@@ -3,9 +3,52 @@
 //`FileManager` class and demonstrate reading data from a file, handling exceptions.
 
 #include<iostream>
+#include<fstream>
 using namespace std;
+class FileManager
+{
+    string data;
+    public :
+
+    void readFileData()
+    {
+
+        ofstream fout;
+        fout.open("demo.txt");
+        getline(cin,data);
+        fout << "Fout use in coding" << endl;
+
+        ifstream fin;
+        fin.open("demo1.txt");
+
+        try
+        {
+            if(fin)
+            {
+                while(getline(fin,data))
+                {
+                    cout << data << endl;
+                }
+            }
+            else
+            {
+                throw runtime_error("file not opened !");
+            }
+        }catch(runtime_error error)
+        {
+            cout << error.what() << endl;
+        }
+        catch(...)
+        {
+            cout << "default exception !!" << endl;
+        }
+
+    }
+};
 
 int main()
 {
-	return 0;
+    FileManager fm;
+    fm.readFileData();
+    return 0;
 }
