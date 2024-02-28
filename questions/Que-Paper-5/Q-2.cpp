fd8d658abc73f62e07766f00e4cea783591c6021
Mayuri Purohit <149376263+mayuuu05@users.noreply.github.com>
2024-02-28 22:34:41 +0530

    Update Q-2.cpp

--- questions/Que-Paper-5/Q-2.cpp
+++ questions/Que-Paper-5/Q-2.cpp
@@ -1,19 +1,54 @@
-//2. Define a class `BankAccount` with private attributes `accountNumber`, `balance`, 
-//and `ownerName`. Encapsulate these attributes using appropriate access specifiers.
-// Implement public member functions to credit, debit, and display the balance. 
-// Demonstrate encapsulation by interacting with the class through its member functions.
+//2. Define a class named `BankAccount` with private attributes `accountNumber`, `balance`, and `ownerName`.
+// Encapsulate these attributes using appropriate access 
+//specifiers. Implement public member functions to credit, debit, and display the balance. Demonstrate encapsulation by 
+//interacting with the class through its member functions.
 
 #include<iostream>
 using namespace std;
-s
+
 class BankAccount
 {
-	private :
-		long long accountNumber ;
-		int balance;
-		string ownerName;
+	long long accountNumber=231155226612;
+	int balance=1000;
+	string ownerName="Mayuri purohit";
+	int addbalance ;
+	int debbalance ;
+	public:
+	void credit()
+	{
+		
+		int addMoney;
+		cout << "Enter addMoney : ";
+		cin >> addMoney;
+		
+		 addbalance = balance + addMoney;
+	}
+	void debit()
+	{
+		
+		int Money;
+		cout << "Enter debMoney : ";
+		cin >> Money;
+		
+		debbalance = balance - Money;
+	}
+	void display()
+	{
+		cout << "Owner Name    : " << ownerName << endl;
+		cout << "Accout number : " << accountNumber << endl;
+		cout << "Balance       : " << balance << endl;
+		cout << "Credit money  : " << addbalance << endl;
+		cout << "debit money  : " << debbalance << endl;
+		
+	}
 };
+
 int main()
 {
+
+	BankAccount b1;
+	b1.credit();
+	b1.debit();
+	b1.display();
 	return 0;
 }
