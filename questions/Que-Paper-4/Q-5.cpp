238a6e33aab4af636d74ba717c0437b8b78cd142
Mayuri Purohit <149376263+mayuuu05@users.noreply.github.com>
2024-02-28 21:16:23 +0530

    Update Q-5.cpp

--- questions/Que-Paper-4/Q-5.cpp
+++ questions/Que-Paper-4/Q-5.cpp
@@ -1,3 +1,56 @@
 //5. Define a class `MathOperations` with a method that performs division. Implement exception handling to handle
 // the case of division by zero and negative numbers.
 // Create an object of the `MathOperations` class and demonstrate exception handling for different scenarios.
+
+#include<iostream>
+using namespace std;
+
+class MathOperations
+{
+	int a,b;
+	public:
+		void set()
+		{
+			cout << "Enter value of a : ";
+			cin >> a;
+			
+			cout << "Enter value of b : ";
+			cin >> b;
+			
+		}
+		void get()
+		{
+			try
+	 		{
+		 		if(b==0 && b<0 && a<0)
+		 		{
+					cout <<  "Divison is : " << a/b << endl;
+					
+	
+				}	
+				
+				else
+				{
+					throw b;
+				}
+			
+			}
+			 catch(...)
+			 {
+			 	cout << "Value can't divide by Zero !!!" << endl;
+			 	cout << "Negative numbers are not allowed !!!" << endl;
+			 
+			 }
+		}
+	 	
+		 	
+		 
+};
+int main()
+{
+	MathOperations m1;
+	m1.set();
+	m1.get();
+	
+	return 0;
+}
