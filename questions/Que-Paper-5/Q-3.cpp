9c602ac35e07a6202f1b9770e38fbacef42e8066
Mayuri Purohit <149376263+mayuuu05@users.noreply.github.com>
2024-02-28 22:37:21 +0530

    Update Q-3.cpp

--- questions/Que-Paper-5/Q-3.cpp
+++ questions/Que-Paper-5/Q-3.cpp
@@ -3,39 +3,80 @@
 //  Derive two classes, `Circle` and `Rectangle`, from the `Shape` class.
 //   Implement methods to calculate the area specific to each shape.
 //    Demonstrate abstraction by calling the area calculation methods for both `Circle` and `Rectangle`.
-
 #include<iostream>
 using namespace std;
 
 class Shape
 {
-	private :
-		string color;
-		int area;
-		
-		public :
-			void setting()
-			{
-				cin.ignore();
-				cout << "Enter color : ";
-				getline(cin,color);
-			}
-			
-			void getting()
-			{
-				
-			}
-		
+	protected :
+	string color;
+	int area;
+	virtual void  calculation ()=0;
+	
+	
+	public :
+		void setting()
+		{
+		 cout <<endl <<"Enter colour of shape :";
+		 cin >> color;
+		 	
+		}
+		void getting()
+		{
+		cout<< "color is : "<< color << endl;	
+		}
 };
+
 class Circle : public Shape
 {
-	
+
+    
+	int r;
+	const float pi=3.14;
+	public:
+	void   calculation ()
+	{
+		cout  << endl << "\t[Circle]"<< endl ;
+		setting();
+		cout << "Enter radius : ";
+		cin >> r;
+		getting();
+		area=pi*r*r;
+		cout<<"circle area is :" << area;
+		
+	}
+
 };
+
 class Rectangle : public Shape
 {
+	int l;
+	int w;
+	public:
+		
+	void   calculation ()
+	{
+		cout  << endl << endl<< "\t[Rectangle]"<< endl ;
+		setting();
+		cout << "Enter length : ";
+		cin >> l;
+		cout << "Enter width : ";
+		cin >> w;
+		
+		area=l*w;
+		getting();
+		cout<<"Rectangle area is :" << area;
 	
+	}
 };
+
 int main()
-{
+{	
+  	Circle c1;
+  	c1.calculation();
+  	
+  	Rectangle r1;
+  	r1.calculation();
+	
 	return 0;
 }
