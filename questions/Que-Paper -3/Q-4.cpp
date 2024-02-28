5e132df0a2ed26513d1eed0b3745c6168c2c9275
Mayuri Purohit <149376263+mayuuu05@users.noreply.github.com>
2024-02-28 19:58:50 +0530

    Update Q-4.cpp

--- questions/Que-Paper -3/Q-4.cpp
+++ questions/Que-Paper -3/Q-4.cpp
@@ -6,7 +6,45 @@
 #include<iostream>
 using namespace std;
 
+class Animal
+{
+	public :
+	virtual void displayDetails()=0;
+	
+};
+class Dog : public Animal
+{
+	public :
+	virtual void displayDetails()
+	{
+		cout << "Dog sound barks !!" << endl;
+		
+	}
+};
+class Cat : public Animal
+{
+	public:
+	virtual void displayDetails()
+	{
+		cout << "Cat sound meow !!" << endl;
+	}
+};
+
 int main()
 {
+	Animal *a1[2];
+	Dog d1;
+	Cat c1;
+	
+	a1[0] = &d1;
+	a1[1]= &c1;
+	
+	for(int i =0; i<2 ; i++)
+	{
+		a1[i]->displayDetails();
+		
+	}
+	
+	
 	return 0;
 }
