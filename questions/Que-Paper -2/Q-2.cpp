9f29917ba7fdefb4d64e5b780f804a41c1d824b2
Mayuri Purohit <149376263+mayuuu05@users.noreply.github.com>
2024-02-28 15:29:54 +0530

    Update Q-2.cpp

--- questions/Que-Paper -2/Q-2.cpp
+++ questions/Que-Paper -2/Q-2.cpp
@@ -3,3 +3,52 @@
 //specifiers. Implement public member functions to credit, debit, and display the balance. Demonstrate encapsulation by 
 //interacting with the class through its member functions.
 
+#include<iostream>
+using namespace std;
+
+class BankAccount
+{
+	long long accountNumber=231155226612;
+	int balance=1000;
+	string ownerName="Mayuri purohit";
+	int addbalance ;
+	int debbalance ;
+	public:
+	void credit()
+	{
+		
+		int addMoney;
+		cout << "Enter addMoney : ";
+		cin >> addMoney;
+		
+		 addbalance = balance + addMoney;
+	}
+	void debit()
+	{
+		
+		int Money;
+		cout << "Enter debMoney : ";
+		cin >> Money;
+		
+		debbalance = balance - Money;
+	}
+	void display()
+	{
+		cout << "Owner Name    : " << ownerName << endl;
+		cout << "Accout number : " << accountNumber << endl;
+		cout << "Balance       : " << balance << endl;
+		cout << "Credit money  : " << addbalance << endl;
+		cout << "debit money  : " << debbalance << endl;
+		
+	}
+};
+
+int main()
+{
+
+	BankAccount b1;
+	b1.credit();
+	b1.debit();
+	b1.display();
+	return 0;
+}
