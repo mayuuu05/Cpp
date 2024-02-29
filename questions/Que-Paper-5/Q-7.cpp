5e8e5c331f5d42ff5184927a8fc3a4e9f4e3ad90
Mayuri Purohit <149376263+mayuuu05@users.noreply.github.com>
2024-02-29 10:13:11 +0530

    Update Q-7.cpp

--- questions/Que-Paper-5/Q-7.cpp
+++ questions/Que-Paper-5/Q-7.cpp
@@ -2,3 +2,57 @@
 //Implement concrete classes `Car` and `Bike` that inherit from `Vehicle`. 
 //Demonstrate polymorphism by creating an array of `Vehicle` pointers, pointing to objects of both `Car`
 // and `Bike`. Call the `startEngine()` and `drive()` functions for each object.
+
+#include<iostream>
+using namespace std;
+
+class Vehicle
+{
+	public :
+		virtual void startEngine()=0;
+		virtual void drive()=0;
+		
+};
+class Car : public Vehicle
+{
+	public:
+	void startEngine()
+	{
+		cout << endl << "Car's engine started!" << endl;
+	}
+	void drive()
+	{
+	 	cout << "Car is being driven." << endl << endl << endl;
+	}
+};
+class Bike : public Vehicle
+{
+	public:
+	void startEngine()
+	{
+		cout << "Bike's engine started." << endl;
+	}
+	void drive()
+	{
+	 	cout << "Bike is being driven." << endl;
+
+	}
+};
+int main()
+{
+	Vehicle *v1[2];
+	
+	Car c1;
+	Bike b1;
+	
+	v1[0]=&c1;
+	v1[1]=&b1;
+	
+	for(int i=0 ;  i<2 ; i++)
+	{
+		v1[i]->startEngine();
+		v1[i]->drive();
+	}
+	
+	return 0;
+}
